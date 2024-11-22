675140ed82622b8ffcb847c41bb97f1b0ca95520
Tim Gover <tim.gover@raspberrypi.com>
2024-11-22 18:06:10 +0000

    win32: Add a help message to the mass-storage-gadget batch file

--- win32/rpi-mass-storage-gadget64.bat
+++ win32/rpi-mass-storage-gadget64.bat
@@ -1,3 +1,9 @@
 @echo off
 @echo USB mass storage gadget for Raspberry Pi 5
 rpiboot -d mass-storage-gadget64
+@echo:
+@echo Raspberry Pi Mass Storage Gadget started
+@echo EMMC/NVMe devices should be visible in the Raspberry Pi Imager in a few seconds.
+@echo For debug, you can login to the device using the USB serial gadget - see COM ports in Device Manager.
+@echo: 
+set /p dummy=Press a key to close this window.
