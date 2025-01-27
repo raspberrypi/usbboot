7e7347f07b2467f6f699403cb8c498ee86c52ddc
Tim Gover <tim.gover@raspberrypi.com>
2025-01-27 13:24:28 +0000

    rpiboot: Add note about rpiboot jumper to the startup guidance

--- main.c
+++ main.c
@@ -964,8 +964,9 @@ int main(int argc, char *argv[])
 
 	get_options(argc, argv);
 
-	printf("RPIBOOT: build-date %s version %s %s\n", __DATE__, PKG_VER, GIT_VER);
-	printf("If the device fails to connect then please see https://rpltd.co/rpiboot for debugging tips.\n");
+	printf("RPIBOOT: build-date %s version %s %s\n\n", __DATE__, PKG_VER, GIT_VER);
+	printf("Please fit the EMMC_DISABLE / nRPIBOOT jumper before connecting the power and USB cables to the target device.\n");
+	printf("If the device fails to connect then please see https://rpltd.co/rpiboot for debugging tips.\n\n");
 
 	// flush immediately
 	setbuf(stdout, NULL);
